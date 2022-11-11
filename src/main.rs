24b0422374345a874411bb80a6d6bc000979ab61
PintaPontus <mateo.blascoviz@gmail.com>
2022-11-11 12:26:14 +0100

    step forward

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,8 @@
-use beryllium::*;
+use winit::event_loop::EventLoop;
+use winit::window::WindowBuilder;
 
 fn main() {
-    let sdl = SDL::init(InitFlags::Everything).expect("couldn't start SDL");
-    sdl.gl_set_attribute(SdlGlAttr::MajorVersion, 3).unwrap();
-    sdl.gl_set_attribute(SdlGlAttr::MinorVersion, 3).unwrap();
-    sdl.gl_set_attribute(SdlGlAttr::Profile, GlProfile::Core).unwrap();
-    #[cfg(target_os = "macos")]
-    {
-        sdl.gl_set_attribute(SdlGlAttr::Flags, ContextFlag::ForwardCompatible).unwrap();
-    }
-    let _win = sdl.create_gl_window(
-            "Hello Window",
-            WindowPosition::Centered,
-            800,
-            600,
-            WindowFlags::Shown,
-        ).expect("couldn't make a window and context");
-    'main_loop: loop {
-        // handle events this frame
-        while let Some(event) = sdl.poll_events().and_then(Result::ok) {
-            match event {
-                Event::Quit(_) => break 'main_loop,
-                _ => (),
-            }
-        }
-        // now the events are clear
-        // here's where we could change the world state and draw.
-    }
+    let event_loop = EventLoop::new();
+    let builder = WindowBuilder::new();
+    builder.build(&event_loop);
 }
