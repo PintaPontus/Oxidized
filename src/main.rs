ecdb871014efc459363cadbaa16872091a87f765
Blasco 001 <matteo.blascoviz@gmail.com>
2022-11-11 13:11:11 +0100

    window example

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use winit::{
     event::{Event, WindowEvent},
     event_loop::EventLoop,
     window::*,
+    dpi::*,
 };
 
 fn main() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
-
+    window.set_title("Oxidized");
     event_loop.run(move |event, _, control_flow| {
         // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
         // dispatched any events. This is ideal for games and similar applications.
@@ -34,6 +35,10 @@ fn main() {
                 // You only need to call this if you've determined that you need to redraw, in
                 // applications which do not always need to. Applications that redraw continuously
                 // can just render here instead.
+
+                let size = window.inner_size();
+                let position = window.inner_position().unwrap();
+                window.set_title(&format!("{} X {} - {} X {}", size.width, size.height, position.x, position.y));
                 window.request_redraw();
             },
             Event::RedrawRequested(_) => {
@@ -42,6 +47,7 @@ fn main() {
                 // It's preferable for applications that do not render continuously to render in
                 // this event rather than in MainEventsCleared, since rendering in here allows
                 // the program to gracefully handle redraws requested by the OS.
+                // println!("RedrawRequested");
             },
             _ => ()
         }
